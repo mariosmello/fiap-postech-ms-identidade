115e375412b7663d85a5d1e6be721c109942e779
Mário Mello <marioesmello@gmail.coim>
2024-07-07 15:31:30 -0300

    Adiciona relatório e cria recurso para solicitação de exclusão de dados.

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,5 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('users/create', [AuthController::class, 'store']);
+    Route::post('users/delete-request', [\App\Http\Controllers\DeleteRequestController::class, 'store']);
 });
